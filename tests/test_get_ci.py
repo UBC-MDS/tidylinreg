2f2803ccf2c457badf54bf68b51f596ddae3904e
mdkbmi <50350226+mdkbmi@users.noreply.github.com>
2025-01-21 23:07:26 -0800

    fix: added missing test for `get_ci` for non-numeric alpha

--- tests/test_get_ci.py
+++ tests/test_get_ci.py
@@ -20,8 +20,16 @@ beta_pred = np.linalg.inv(X.T @ X) @ X.T @ y_true
 y_pred = X * beta_pred
 
 @pytest.fixture
+def empty_model():
+    model = LinearModel()
+    return model
+
+@pytest.fixture
 def test_model():
     model = LinearModel()
+    model.params = beta_pred
+    model.X = X
+    model.y = y_true
     return model
 
 @pytest.fixture
@@ -29,20 +37,24 @@ def ref_model():
     model = sm.OLS(y_true, X).fit()
     return model
 
-def test_is_model_fitted(test_model):
+def test_is_model_fitted(empty_model):
     with pytest.raises(ValueError):
-        test_model.get_ci()
+        empty_model.get_ci()
 
-def test_error_empty_x(test_model):
-    test_model.params = beta_pred
+def test_error_empty_x(empty_model):
+    empty_model.params = beta_pred
     with pytest.raises(ValueError):
-        test_model.get_ci()
+        empty_model.get_ci()
 
-def test_error_empty_y(test_model):
-    test_model.params = beta_pred
-    test_model.X = X
+def test_error_empty_y(empty_model):
+    empty_model.params = beta_pred
+    empty_model.X = X
     with pytest.raises(ValueError):
-        test_model.get_ci()
+        empty_model.get_ci()
+
+def test_alpha_is_non_numeric(test_model):
+    with pytest.raises(TypeError):
+        test_model.get_ci(alpha="hello")        
 
 def test_alpha_is_zero(test_model):
     with pytest.raises(ValueError):
@@ -61,9 +73,6 @@ def test_alpha_above_one(test_model):
         test_model.get_ci(alpha=1.01)
 
 def test_ci_values(test_model, ref_model):
-    test_model.params = beta_pred
-    test_model.X = X
-    test_model.y = y_true
     test_model.test_statistic = ref_model.tvalues
     test_model.std_error = ref_model.bse
 
